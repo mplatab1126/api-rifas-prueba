1be4c4c49c67be25e4aedcf942ffe0500922b537
mplatab1126 <admin@losplatasas.com>
2026-02-20 11:49:13 -0500

    Update abono.js

--- api/admin/abono.js
+++ api/admin/abono.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   const { numeroBoleta, valorAbono, metodoPago, referencia, contrasena, esPendiente } = req.body;
 
-  const asesores = { 'sal32':'Saldarriaga', 'ar94':'Arias', 'car61':'Carlos', 'an45':'Anyeli', 'AYX':'Mateo', 'lu34':'Luisa', 'li05':'Liliana', 'ne26':'Nena', '1234':'Admin' };
+  const asesores = JSON.parse(process.env.ASESORES_SECRETO || '{}');
   const nombreAsesor = asesores[contrasena];
 
   if (!nombreAsesor) return res.status(401).json({ status: 'error', mensaje: 'Contraseña de asesor incorrecta' });
