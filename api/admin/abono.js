c9dfebcbc6169dd0edd03ec480f41474a329f3c1
Mateo Plata <admin@losplatasas.com>
2026-03-16 14:54:00 -0500

    V2

--- api/admin/abono.js
+++ api/admin/abono.js
@@ -51,7 +51,7 @@ export default async function handler(req, res) {
 
     const { data: boletaData, error: boletaError } = await supabase
       .from(tabla)
-      .select('saldo_restante, total_abonado, telefono_cliente, asesor, nombre_cliente')
+      .select('saldo_restante, total_abonado, telefono_cliente, asesor')
       .eq('numero', numeroLimpio)
       .single();
 
